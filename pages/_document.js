f90bd680c0e8bb0f3d42ff555cf00c1426788f64
engin <o.enginoz@gmail.com>
2019-08-29 22:26:37 +0300

    Add styling sheets to the project

--- pages/_document.js
+++ pages/_document.js
@@ -9,74 +9,72 @@ export default class MyDocument extends Document {
 
     render() {
         return (
-            <html lang="en">
-                <Head>
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1"
-                    />
-                    <link rel="icon" href="/static/icons/favicon.ico" />
+          <html lang="en">
+            <Head>
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
+              <link rel="icon" href="/static/icons/favicon.ico" />
 
-                    <link rel="manifest" href="/static/manifest.json" />
-                    <meta
-                        name="theme-color"
-                        content={config.css.primaryColor}
-                    />
+              <link rel="manifest" href="/static/manifest.json" />
+              <meta name="theme-color" content={config.css.primaryColor} />
 
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="/static/icons/icon_32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="48x48"
-                        href="/static/icons/icon_48.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="128x128"
-                        href="/static/icons/icon_128.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="256x256"
-                        href="/static/icons/icon_256.png"
-                    />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/static/icons/icon_32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="48x48"
+                href="/static/icons/icon_48.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="128x128"
+                href="/static/icons/icon_128.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="256x256"
+                href="/static/icons/icon_256.png"
+              />
 
-                    <meta
-                        name="apple-mobile-web-app-title"
-                        content={config.siteName}
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="152x152"
-                        href="/static/icons/icon_180.png"
-                    />
+              <meta
+                name="apple-mobile-web-app-title"
+                content={config.siteName}
+              />
+              <link
+                rel="apple-touch-icon"
+                sizes="152x152"
+                href="/static/icons/icon_180.png"
+              />
 
-                    <meta
-                        name="msapplication-TileColor"
-                        content={config.css.primaryColor}
-                    />
-                    <meta
-                        name="msapplication-TileImage"
-                        content="/static/icons/icon_150.png"
-                    />
-                    {/* 
+              <meta
+                name="msapplication-TileColor"
+                content={config.css.primaryColor}
+              />
+              <meta
+                name="msapplication-TileImage"
+                content="/static/icons/icon_150.png"
+              />
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css" />
+              {/* 
                     <script>
                          Add a Google Analytics script here.
                     </script>
                     */}
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </html>
+            </Head>
+            <body>
+              <Main />
+              <NextScript />
+            </body>
+          </html>
         );
     }
 }
--- styles/index.js
+++ styles/index.js
@@ -20,115 +20,173 @@ export const centeredPageStyles = css`
 
 // used in _app.js
 export const globalStyles = css.global`
-@font-face {
-    font-family: "Abril Fatface";
-    font-display: auto; /* Important for performance */
-    src: url("/static/fonts/Abril-Fatface.woff2") format("woff2");
-    font-style: normal;
-}
-html {
-    scroll-behavior: smooth;
-}
-body {
-    position: relative;
-    top: 0;
-    margin: 0;
-    padding: 0;
-    color: ${config.css.black};
-}
-#__next {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-    min-height: 100vh;
-}
-.footer {
-    background: #fff;
-    height: 80px;
-    width: 100%;
-    border-top: 2px solid ${config.css.lightGray};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 16px;
-    position: absolute;
-    bottom: 0;
-}
-strong,
-h1,
-h1 > *,
-h2,
-h2 > *,
-h3,
-h3 > *,
-h4,
-h4 > *,
-h5,
-h5 > *,
-h6,
-h6 > * {
-    font-family: "Abril Fatface";
-    letter-spacing: 1px;
-}
-strong {
-    color: ${config.css.accentColor}
-}
-.search-layout,
-.blog-layout,
-.blog-post-list {
-    max-width: 740px;
-    padding: 40px 20px 100px 20px;
-    position: relative;
-}
-.blog-layout > div {
-    display: flex;
-    flex-direction: column;
-}
-img {
-    max-width: 100%;
-}
-img.img-centered {
-    align-self: center;
-    margin: 10px 0px;
-}
-.icon-button {
-    outline: none;
-    border: none;
-    background: ${config.css.backgroundColor};
-}
-.icon-button.hamburger {
-    position: fixed;
-    left: 0px;
-    top: 0px;
-    z-index: 3; /* the navigation menu is z-index: 4 */
-    border: 1px solid ${config.css.lightGray};
-    padding-top: 4px;
-    border-radius: 3px;
-}
-a {
-    color: ${config.css.accentColor};
-}
-label {
-    color: ${config.css.black};
-    font-weight: 600;
-    font-family: sans-serif;
-    font-size: 18px;
-}
-p,
-p a,
-b,
-em,
-span,
-li,
-ul,
-ol {
-    font-size: 18px;
-    line-height: 24px;
-    word-break: break-word /* This prevents a bug with the reading progress bar. Leave this style here unless you are removing the reading progress bar. */
-}
+         @font-face {
+           font-family: "Abril Fatface";
+           font-display: auto; /* Important for performance */
+           src: url("/static/fonts/Abril-Fatface.woff2") format("woff2");
+           font-style: normal;
+         }
+         html {
+           scroll-behavior: smooth;
+         }
+         body {
+           position: relative;
+           top: 0;
+           margin: 0;
+           padding: 0;
+           color: ${config.css.black};
+         }
+         #__next {
+           display: flex;
+           flex-direction: column;
+           flex: 1;
+           min-height: 100vh;
+         }
+         .footer {
+           background: #fff;
+           height: 80px;
+           width: 100%;
+           border-top: 2px solid ${config.css.lightGray};
+           display: flex;
+           justify-content: center;
+           align-items: center;
+           font-size: 16px;
+           position: absolute;
+           bottom: 0;
+         }
+         strong,
+         h1,
+         h1 > *,
+         h2,
+         h2 > *,
+         h3,
+         h3 > *,
+         h4,
+         h4 > *,
+         h5,
+         h5 > *,
+         h6,
+         h6 > * {
+           font-family: "Abril Fatface";
+           letter-spacing: 1px;
+         }
+         strong {
+           color: ${config.css.accentColor};
+         }
+         .search-layout,
+         .blog-layout,
+         .blog-post-list {
+           max-width: 740px;
+           padding: 40px 20px 100px 20px;
+           position: relative;
+         }
+         .blog-layout > div {
+           display: flex;
+           flex-direction: column;
+         }
+         img {
+           max-width: 100%;
+         }
+         img.img-centered {
+           align-self: center;
+           margin: 10px 0px;
+         }
+         .icon-button {
+           outline: none;
+           border: none;
+           background: ${config.css.backgroundColor};
+         }
+         .icon-button.hamburger {
+           position: fixed;
+           left: 0px;
+           top: 0px;
+           z-index: 3; /* the navigation menu is z-index: 4 */
+           border: 1px solid ${config.css.lightGray};
+           padding-top: 4px;
+           border-radius: 3px;
+         }
+         a {
+           color: ${config.css.accentColor};
+         }
+         label {
+           color: ${config.css.black};
+           font-weight: 600;
+           font-family: sans-serif;
+           font-size: 18px;
+         }
+         p,
+         p a,
+         b,
+         em,
+         span,
+         li,
+         ul,
+         ol {
+           font-size: 18px;
+           line-height: 24px;
+           word-break: break-word; /* This prevents a bug with the reading progress bar. Leave this style here unless you are removing the reading progress bar. */
+         }
 
-code > span {
-    font-size: inherit; /* make sure code font size is set in <CodeBlock /> */
-    background: inherit !important; /* Fixes code block style bug*/
-}
-`;
+         code > span {
+           font-size: inherit; /* make sure code font size is set in <CodeBlock /> */
+           background: inherit !important; /* Fixes code block style bug*/
+         }
+
+         blockquote {
+           display: block;
+           background: #fff;
+           padding: 15px 20px 15px 45px;
+           margin: 0 0 20px;
+           position: relative;
+
+           font-family: Georgia, serif;
+           font-size: 14px;
+           line-height: 1.2;
+           color: #666;
+
+           -moz-box-shadow: 2px 2px 15px #ccc;
+           -webkit-box-shadow: 2px 2px 15px #ccc;
+           box-shadow: 2px 2px 15px #ccc;
+
+           border-left-style: solid;
+           border-left-width: 15px;
+           border-right-style: solid;
+           border-right-width: 2px;
+
+           border-left-color: #5e9de6;
+           border-right-color: #4b8ad6;
+         }
+
+         blockquote::before {
+           content: "''";
+
+           font-family: Georgia, serif;
+           font-size: 60px;
+           font-weight: bold;
+           color: #999;
+
+           position: absolute;
+           left: 10px;
+           top: 5px;
+         }
+
+         blockquote::after {
+           content: "";
+         }
+
+         blockquote a {
+           text-decoration: none;
+           background: #eee;
+           cursor: pointer;
+           padding: 0 3px;
+           color: #c76c0c;
+         }
+
+         blockquote a:hover {
+           color: #666;
+         }
+
+         blockquote em {
+           font-style: italic;
+         }
+       `;
